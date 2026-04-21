43aa51f6698b8ea4d4d52edc20d38dbb35c5b956
Yi Ming <ofseed@foxmail.com>
2026-04-21 23:47:37 +0800

    feat(json): add generic JSON RPC client/server

--- src/gen/gen_vimdoc.lua
+++ src/gen/gen_vimdoc.lua
@@ -204,6 +204,7 @@ local config = {
       'version.lua',
 
       -- Sections at the end, in a specific order:
+      'rpc.lua',
       'ui2.lua',
     },
     files = {
@@ -226,6 +227,7 @@ local config = {
       'runtime/lua/vim/glob.lua',
       'runtime/lua/vim/hl.lua',
       'runtime/lua/vim/iter.lua',
+      'runtime/lua/vim/json/rpc.lua',
       'runtime/lua/vim/keymap.lua',
       'runtime/lua/vim/loader.lua',
       'runtime/lua/vim/net.lua',
@@ -267,6 +269,8 @@ local config = {
         return 'LUA-VIMSCRIPT BRIDGE'
       elseif name == 'builtin' then
         return 'VIM'
+      elseif name == 'rpc' then
+        return 'Lua module: vim.json.rpc'
       elseif name == 'ui2' then
         return 'UI2'
       end
@@ -279,6 +283,8 @@ local config = {
         return 'lua-vim-system'
       elseif name == 'Options' then
         return 'lua-vimscript'
+      elseif name == 'Rpc' then
+        return 'json-rpc'
       elseif name == 'ui2' then
         return 'ui2'
       end
